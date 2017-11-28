058bd3d690bef5f01da29a380a4eba47d60fe725
Sarah Joy Calpo <sajoy.pc@gmail.com>
2017-11-28 12:57:33 -0800

    update demo

--- 04-templating/demo/pets.js
+++ 04-templating/demo/pets.js
@@ -1,18 +1,21 @@
 const petObjects = [
     {
         name: 'bob',
+        sound: '<h1>ROAR</h1>',
         type: 'bear',
         description: 'used to be blue, now is red; likes to read before bed',
         image: 'https://media.giphy.com/media/uUs14eCA2SBgs/giphy.gif'
     },
     {
         name: 'sally',
+        sound: '<h1>ROAR</h1>',
         type: 'doge',
         description: 'favorite musical artist is sheryl crow',
         image: 'https://media.giphy.com/media/mbeBWFPtqdq36/giphy.gif'
     },
     {
         name: 'reba',
+        sound: '<h1>MEOW</h1>',
         type: 'cat',
         description: 'prefers rose gold',
         image: 'https://media.giphy.com/media/wrXXI5XwIx6c8/giphy.gif'
--- 04-templating/demo/scripts.js
+++ 04-templating/demo/scripts.js
@@ -1,5 +1,70 @@
 //                                                                  ARROW FUNCTIONS
 
+function rain () {
+    console.log('`');
+}
+
+rain();
+
+let rainMore = function () {
+    console.log('`');
+    console.log('`');   
+};
+
+rainMore();
+
+let sunshine = (time,degree) => {
+    console.log(time, degree);
+};
+
+sunshine('5pm',30);
+
+let storm = () => {
+    for (let i = 0; i < 20; i++ ) {
+        console.log('`');        
+    }
+};
+
+
+
+let plants = ['spider','moses','ivy'];
+
+plants.forEach(function(plant){
+    console.log('using regular function:', plant);
+    console.log('THIS IS:', this );
+});
+
+// plants.forEach((plant) => { console.log('using full punctuation arrow', plant); });
+// plants.forEach(plant => { console.log('using no () arrow',plant); });
+// plants.forEach( plant => console.log('using no punctuation arrow', plant) );
+
+plants.forEach((plant) => { 
+    console.log('using full punctuation arrow', plant); 
+    console.log('THIS IS:', this);
+});
+
+console.log('THIS IS', this); // this === WINDOW
+
+const codeSchool = {
+    students: ['bob','jean','louis'],
+    classes: ['flute','flambe','improv'],
+    addStudent: function (name) {
+        console.log('THIS IS', this); // this === codeSchool
+        this.students.push(name);
+    },
+    addClass: className => {
+        console.log('THIS IS', this); // this === WINDOW
+        this.classes.push(className);
+    }
+};
+
+
+
+
+
+
+
+
 
 
 
@@ -11,18 +76,39 @@ const pets = [];
 function Pet ( petData ) {
     this.name = petData.name;
     this.type = petData.type;
+    this.sound = petData.sound;
     this.description = petData.description;
     this.image = petData.image;
 }
 
 Pet.prototype.toHtml = function () {
-    // TODO use Handlebars to compile our HTML template instead of cloning it w jQuery
-    var $newPet = $( 'article.template' ).clone().removeClass( 'template' );
-    $newPet.find( 'h2' ).text( this.name );
-    $newPet.find( 'p' ).text( this.description );
-    $newPet.find( 'img' ).attr( 'src', this.image );
+    // get the template to use
+    const petTemplate = $('#pet-template').html();
+    /*
+      <h2>{{name}}</h2>
+      <h3>{{type}}</h3>
+      <p>{{description}}</p>
+      <img src='{{image}}'>
+      {{imgTag}}
+    */
 
-    return $newPet;
+    // compile the template into a JS function
+    const templateFiller = Handlebars.compile(petTemplate);
+
+    // const filledTemplate = templateFiller({name:'Louis', type:'DOGE',description:'cute'});
+    const filledTemplate = templateFiller(this);
+
+    return filledTemplate;
+
+    // use that function to create a filled in template
+
+    // TODO use Handlebars to compile our HTML template instead of cloning it w jQuery
+    // const $newPet = $( 'article.template' ).clone().removeClass( 'template' );
+    // $newPet.find( 'h2' ).text( this.name );
+    // $newPet.find( 'p' ).text( this.description );
+    // $newPet.find( 'img' ).attr( 'src', this.image );
+
+    // return $newPet;
 };
 
 function populatePets () {
