c82f9e59d1ea40b5f8b957700bf86a2ea4cf6441
Sarah Joy Calpo <sajoy.pc@gmail.com>
2017-12-05 10:14:04 -0800

    update demo day 9

--- 09-sql-joins-relations/demos/node-pg/server.js
+++ 09-sql-joins-relations/demos/node-pg/server.js
@@ -1,10 +1,13 @@
 'use strict';
 
-const pg = require('pg');
-const fs = require('fs');
+const pg = require('pg'); 
+// pg is the npm module that allows us to send a query from our server to our database
+const fs = require('fs'); 
+// fs is the npm module that allows us to interface (do stuff) with the server's files
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+// process.env.PORT will be used during production
 
 const client = new pg.Client('postgres://postgres:1234@localhost:5432/demos');
 client.connect();
@@ -28,7 +31,7 @@ app.get('/tasks', function(request, response) {
 });
 
 app.get('/tasks/chores', function(request, response) {
-    client.query('SELECT * FROM tasks WHERE category = "chores";')
+    client.query(`SELECT * FROM tasks WHERE category ='chores';`)
         .then(function(data) {
             response.send(data.rows);
         })
@@ -38,8 +41,32 @@ app.get('/tasks/chores', function(request, response) {
 
 });
 
+app.get('/tasks/works', function(request, response) {
+    client.query(`SELECT * FROM tasks WHERE category ='work';`)
+        .then(function(data) {
+            response.send(data.rows);
+        })
+        .catch(function(err) {
+            console.error(err);
+        });
+
+});
+
+app.get('/tasks/:category', function(request, response) {
+    console.log(request.params);
+    // client.query(`SELECT * FROM tasks WHERE category ='${request.params.category}';`)
+    client.query(`SELECT * FROM tasks WHERE category =$1;`, [request.params.category])
+        .then(function(data) {
+            response.send(data.rows);
+        })
+        .catch(function(err) {
+            console.error(`request /tasks/:category ---- ${err}`);
+        });
+
+});
+
 app.get('/categories', function (request, response) {
-    client.query('SELECT categories FROM tasks;')
+    client.query('SELECT category FROM tasks;')
         .then(function (data) {
             response.send(data.rows);
         })
@@ -58,8 +85,11 @@ app.get('/users', function (request, response) {
         });
 });
 
+// :username is a placeholder for whatever is input in the url
+// ie /users/mickey/tasks is stored in request.params like this: { username: 'mickey'}
 app.get('/users/:username/tasks', function(request, response) {
-    console.log(request.params);
+    console.log(request.params.username);
+    // $1 is the placeholder for the first item of the second parameter's array
     client.query('SELECT * FROM tasks WHERE person = $1;', [request.params.username])
         .then(function(data) {
             response.send(data.rows);
